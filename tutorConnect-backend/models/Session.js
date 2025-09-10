ba215286f9a8b5131077613cbba0bef7d6253355
aishaomaa <124359762+Aishagojo@users.noreply.github.com>
2025-09-10 22:23:55 +0300

    Update Session.js

--- tutorConnect-backend/models/Session.js
+++ tutorConnect-backend/models/Session.js
@@ -1,5 +1,5 @@
 
-const mongoose = require('mongoose');
+const h mongoose = require('mongoose');
 
 const sessionSchema = new mongoose.Schema({
   student: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
